3ba472b68dd0305c83373c8caa0ecdfc08ed3122
Kewin Titus <kewintitus@gmail.com>
2022-10-06 18:16:54 +0530

    Add project files.

---
+++ PointCloudToggle/Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB.PointClouds;
+
+namespace PointCloudToggle
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    public class Class1 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+            PointCloudOverrides pointCloudOverrides = doc.ActiveView.GetPointCloudOverrides();
+            PointCloudOverrideSettings settings = new PointCloudOverrideSettings();
+            
+            FilteredElementCollector pointClouds = new FilteredElementCollector(doc);
+            List<ElementId> pointCloudId = new List<ElementId>();
+            ElementCategoryFilter pointCloudfilter = new ElementCategoryFilter(BuiltInCategory.OST_PointClouds);
+            IList<Element> listPointClouds = pointClouds.WherePasses(pointCloudfilter).WhereElementIsNotElementType().ToElements();
+            View currentView = uidoc.ActiveView;
+            if (listPointClouds.Count==0)
+            {
+                IEnumerable<ElementId> hiddenPointClouds = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_PointClouds)
+                    .WhereElementIsNotElementType().Where(x => x.IsHidden(currentView))
+                .Select(x => x.Id);
+
+                foreach(ElementId hiddenPointCloud in hiddenPointClouds)
+                {
+                    Element pC = doc.GetElement(hiddenPointCloud);
+                    listPointClouds.Add(pC);
+                }
+                
+            }
+            foreach(Element pc in listPointClouds)
+            {
+                settings = pointCloudOverrides.GetPointCloudScanOverrideSettings(pc.Id);
+                pointCloudId.Add(pc.Id);
+            }
+           
+            try
+            {
+
+                using (Transaction transaction = new Transaction(doc, "PointCloudToggle"))
+                {
+                    transaction.Start();
+                    //if (currentView.IsInTemporaryViewMode(TemporaryViewMode.RevealHiddenElements))
+                    if (settings.Visible == false)
+                    {
+                        settings.Visible = true;
+                        
+                    }
+                    else 
+                    {
+                        settings.Visible = false; 
+                    }
+                    foreach (ElementId pId in pointCloudId)
+                    {
+                        pointCloudOverrides.SetPointCloudScanOverrideSettings(pId, settings,"ToggleONorOFF",doc);
+                    }
+                    transaction.Commit();
+                }
+            }
+            catch (Exception e )
+            {
+                message = e.Message;
+            }
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ PointCloudToggle/ExternalApp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+using Autodesk.Revit.UI;
+
+namespace PointCloudToggle
+{
+    class ExternalApp:IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            application.CreateRibbonTab("PointCloud_Tools");
+
+            string path = Assembly.GetExecutingAssembly().Location;
+            PushButtonData button = new PushButtonData("Toggle_Button", "PointCloud ON/OFF", path, "PointCloudToggle.Class1");
+
+            RibbonPanel panel = application.CreateRibbonPanel("PointCloud_Tools", "PointCloudON/OFF");
+
+            // string iconName = "Plugin001.Images.3DBoxIcon.png";
+            //string iconPath = Path.GetFullPath(iconName); 
+
+            Uri imagePath = new Uri(@"pack:\\application:,,,/PointCloudToggle;component/Images/PC_ICON_1.png");
+            BitmapImage image = new BitmapImage(imagePath);
+
+            PushButton pushButton = panel.AddItem(button) as PushButton;
+            pushButton.LargeImage = image;
+
+            return Result.Succeeded;
+        }
+    }
+}
